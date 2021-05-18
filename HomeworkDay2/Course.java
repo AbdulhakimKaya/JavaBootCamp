1cd56f5f3d7316023118d7b2d66a4e22007b1646
Abdulhakim KAYA <73741703+KAYA-99@users.noreply.github.com>
2021-05-19 01:19:40 +0300

    Homework on Day 2

---
+++ HomeworkDay2/Course.java
@@ -0,0 +1,16 @@
+package odev2.Gun;
+
+public class Course {
+
+    int id;
+    String courseName;
+    String teacherName;
+    double unitPrice;
+
+    public Course(int id, String courseName, String teacherName, double unitPrice) {
+        this.id = id;
+        this.courseName = courseName;
+        this.teacherName = teacherName;
+        this.unitPrice = unitPrice;
+    }
+}
---
+++ HomeworkDay2/CourseManager.java
@@ -0,0 +1,11 @@
+package odev2.Gun;
+
+public class CourseManager {
+
+    public void findACourse() {
+        System.out.println("Kurs Bul butonu calistirildi. Aradiginiz kursun adini yazabilirsiniz...");
+    }
+    public void beIncludedInTheProgram(Course course) {
+        System.out.println("Kayit ol butonu calistirildi");
+    }
+}
