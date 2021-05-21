b4ff7be2c970cfb13b62712a09f054077b08a815
Abdulhakim KAYA <73741703+KAYA-99@users.noreply.github.com>
2021-05-21 13:47:28 +0300

    Homework on Day2

---
+++ HomeworkDay2/Course.java
@@ -0,0 +1,16 @@
+package HomeworkDay2;
+
+public class Course {
+
+    int id;
+    String courseName;
+    String teacherName;
+    double unitPrice;
+
+    public Course(int id, String courseName, String teacherName, double unitPrice) {
+        this.id = id;
+        this.courseName = courseName;
+        this.teacherName = teacherName;
+        this.unitPrice = unitPrice;
+    }
+}
