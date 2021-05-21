ff5bae88286fcc7c9f77e5806491361067baaa74
Abdulhakim KAYA <73741703+KAYA-99@users.noreply.github.com>
2021-05-21 13:50:37 +0300

    Homework on Day 3

---
+++ HomeworkDay3/StudentUserManager.java
@@ -0,0 +1,4 @@
+package HomeworkDay3;
+
+public class StudentUserManager {
+}
---
+++ HomeworkDay3/User.java
@@ -0,0 +1,9 @@
+package HomeworkDay3;
+
+public class User {
+    int id;
+    String firstName;
+    String lastName;
+    String email;
+    String password;
+}
