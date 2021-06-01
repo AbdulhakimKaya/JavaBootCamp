cee002af5ebe7fa90f26ccdf3a24d3889223beb0
Abdulhakim KAYA <73741703+KAYA-99@users.noreply.github.com>
2021-06-01 23:52:19 +0300

    Delete HomeworkDay5/HomeworkDay5 directory

--- HomeworkDay5/HomeworkDay5/eCommerce/core/AuthService.java
+++
@@ -1,6 +0,0 @@
-package HomeworkDay5.eCommerce.core;
-
-public interface AuthService {
-    void login(String email,String password);
-    void register(int id, String firstName, String lastName, String eMail, String password);
-}
